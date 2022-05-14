9c847228690ce14e699df6bf768f42bfd6577c2c
Esteban015 <estebanamaya015@gmail.com>
2022-05-13 21:50:24 -0300

    Add files via upload

--- Test Suites/Mercadolibre/Moda.ts
+++ Test Suites/Mercadolibre/Moda.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e1a7052a-59f1-4f82-86a1-724bc3626654</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb2d41d2-fa5d-44b8-8f8a-6a26ac84b597</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mercadolibre/Seleccionar un Producto de Moda</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
